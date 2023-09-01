01a1af8458bfce253db627fe4cec3fd5c2ebe6fa
Arlon <arlon@getbamba.com>
2023-09-01 05:52:14 +0400

    Splashscreen backbone with basic theming

--- app/src/main/java/arlon/portfolio/palabras/MainActivity.kt
+++ app/src/main/java/arlon/portfolio/palabras/MainActivity.kt
@@ -3,13 +3,25 @@ package arlon.portfolio.palabras
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import arlon.portfolio.palabras.ui.theme.DarkRed
 import arlon.portfolio.palabras.ui.theme.PalabrasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen()
                 }
             }
         }
@@ -30,17 +42,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_conversation),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .aspectRatio(1.0f)
+        )
+        
+        Text(
+            text = "Palabras",
+            modifier = Modifier.padding(top = 12.dp),
+            style = MaterialTheme.typography.titleLarge.copy(
+                color = DarkRed,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SplashPreview() {
     PalabrasTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
